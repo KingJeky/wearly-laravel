53510f8e89f94f1153d12c7f61ca005f1559708f
bagas <xnfcp091@gmail.com>
2025-10-03 14:50:30 +0700

    livewire bagas sadboy

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,11 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controller\SessionController;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/sesi', [SessionController::class, 'index']); 
+Route::post('/sesi/login', [SessionController::class, 'login']); 
